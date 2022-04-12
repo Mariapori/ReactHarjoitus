a2b81c1cd1d56f2ea90a4c09d91d653764314428
Topias Mariapori <topias@mariapori.fi>
2022-04-12 23:43:38 +0300

    Alku duuneja

--- Client/reactharjoitus.client/src/App.js
+++ Client/reactharjoitus.client/src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
 
 function App() {
+  const [henkilo, setHenkilo] = useState("");
+
+  function AsetaHenkilo(henkilonNimi){
+    setHenkilo(henkilonNimi);
+    console.log("Asetettu: " + henkilonNimi);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Simple CRUD</h1>
+      <div>
+        <label>Nimi: </label>
+        <input onChange={(e) => AsetaHenkilo(e.target.value)} value={henkilo}></input>
+      </div>
     </div>
   );
 }
+
 
 export default App;
